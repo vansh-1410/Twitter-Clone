81bfe8ec6410dc31e9a9bbd10dc74e5d1592c5b4
Vansh Sahu <vanshsahu2004sgr@gmail.com>
2025-04-29 17:54:45 +0530

    Initial commit - Twitter clone project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes"
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // <-- React/Vite ke liye
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [daisyui],
+
+    daisyui: {
+      themes: [
+        "light",
+        {
+          black: {
+            ...daisyUIThemes["black"],
+            primary: "rgb(29, 155, 240)",
+            secondary: "rgb(24, 24, 24)",
+          },
+        },
+      ],
+    },
+
+  }
+  
